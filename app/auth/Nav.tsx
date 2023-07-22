30c1fc2892b217964ec8184879897d1e9167bf60
daiyan1998 <abduddaiyan7@gmail.com>
2023-07-22 18:25:06 +0600

    added sign in and signout options

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 import Login from "./Login";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
-console.log("object");
+import Logged from "./Logged";
 
 const Nav = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
+  console.log("object");
 
   return (
     <nav className="flex justify-between items-center py-8">
@@ -15,7 +16,8 @@ const Nav = async () => {
         <h1 className="font-bold text-lg">Send it.</h1>
       </Link>
       <ul className="flex items-center gap-6">
-        <Login />
+        {!session?.user && <Login />}
+        {session?.user && <Logged image={session.user?.image || ""} />}
       </ul>
     </nav>
   );
